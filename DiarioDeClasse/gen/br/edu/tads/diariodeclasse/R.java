e430e16615f1c94db290bccc78caea50b2cec145
MaikeJordan <maikejrs@hotmail.com>
2014-10-03 16:20:26 -0300

    Tela Disciplina

--- DiarioDeClasse/gen/br/edu/tads/diariodeclasse/R.java
+++ DiarioDeClasse/gen/br/edu/tads/diariodeclasse/R.java
@@ -24,18 +24,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int btnAdicionar=0x7f080004;
+        public static final int edtCargaHoraria=0x7f080003;
+        public static final int edtNome=0x7f080001;
+        public static final int txtCargaHoraria=0x7f080002;
+        public static final int txtNome=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_cadastro_disciplina=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int cadastro_disciplina=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btnAdicionar=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int txtCargaHoraria=0x7f050004;
+        public static final int txtNome=0x7f050003;
     }
     public static final class style {
         /** 
---
+++ DiarioDeClasse/src/br/edu/tads/diariodeclasse/CadastroDisciplina.java
@@ -0,0 +1,22 @@
+package br.edu.tads.diariodeclasse;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CadastroDisciplina extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastro_disciplina);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.cadastro_disciplina, menu);
+		return true;
+	}
+
+}
