9b42c2ef09a137b86669fadc140f78b26fef4e7b
Aricelio <ariceliosouza@hotmail.com>
2014-10-10 16:49:58 -0300

    Criação da tela ListagemDisciplina.java

--- DiarioDeClasse/gen/br/edu/tads/diariodeclasse/R.java
+++ DiarioDeClasse/gen/br/edu/tads/diariodeclasse/R.java
@@ -24,28 +24,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080008;
         public static final int btnAdicionar=0x7f080004;
-        public static final int btnCadastroAluno=0x7f080005;
-        public static final int btnCadastroDisciplina=0x7f080006;
+        public static final int btnCadastroAluno=0x7f080006;
+        public static final int btnCadastroDisciplina=0x7f080007;
         public static final int edtCargaHoraria=0x7f080003;
         public static final int edtNome=0x7f080001;
+        public static final int lstDisciplinas=0x7f080005;
         public static final int txtCargaHoraria=0x7f080002;
         public static final int txtNome=0x7f080000;
     }
     public static final class layout {
         public static final int activity_cadastro_disciplina=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_listagem_disciplina=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int cadastro_disciplina=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int listagem_disciplina=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int btnAdicionar=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_listagem_disciplina=0x7f050006;
         public static final int txtCargaHoraria=0x7f050004;
         public static final int txtNome=0x7f050003;
     }
